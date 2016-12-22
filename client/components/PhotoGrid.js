043f25b96e641206de6775de4f64cdc296e21159
Malinda Wellalage <malinda.iw@gmail.com>
2016-12-22 10:33:26 +0530

    Integrate redux

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PhotoGrid = React.createClass({
+  render() {
+    return (
+      <div>
+        Im the PhotoGrid
+      </div>
+    )
+  }
+})
+
+export default PhotoGrid;
---
+++ client/components/Single.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Single = React.createClass({
+  render() {
+    return (
+      <div>
+        Im the Single
+      </div>
+    )
+  }
+})
+
+export default Single;
